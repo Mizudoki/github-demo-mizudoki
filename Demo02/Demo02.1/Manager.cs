bc9bac74a136ac14fffd1dd6d716725917e38545
Mizudoki <168154304+Mizudoki@users.noreply.github.com>
2025-05-25 21:11:08 +0700

    First_Time_Test_Git

---
+++ Demo02/Demo02.1/Manager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo02._1
+{
+    // Class Mangager se ke thua class Employee
+    public class Manager : Employee, IWork, IPayable
+    {
+        // (Abstract) class:
+        // Is a: Manager "is a" Employee
+
+        // Interface:
+        // Can do: Manager "can do" work, Manager "can do" pay
+        public string Email { get; set; }
+
+        // Lop con se co tat ca thuoc tinh, phuong thuc cua lop cha
+        public Manager() 
+        { 
+        }
+
+        // base: Dung de chi lop cha(lop co so), giong super ben Java
+        public Manager(int id, string name) : base(id, name)
+        {
+        }
+
+        public Manager(int id, string name, string address, int age) : base(id, name, address, age)
+        {
+        }
+
+        public Manager(int id, string name, string address, int age, string email) : base(id, name, address, age)
+        {
+            Email = email;
+        }
+
+        public override string ToString()
+        => $"Id: {Id} - Name: {Name} - Address: {Address} - Age: {Age}"
+            + $"- Email: {Email}";
+
+        public void Pay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Demo02/Demo02.2/GenericClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo02._2
+{
+    // Generic: Tong quat, chung chung...
+    // Generic Class: Mot class voi kieu du lieu tong quat
+    // Cach dung: them <T> dang sau, T: Type
+
+    // Constraint: Rang buoc
+    // Cach dung: Them where T: {Dieu kien rang buoc} vao sau <T>
+    // where T : struct
+    // Bat buoc T la kieu so (Tham tri)
+    // where T : class
+    // Bat buoc T la kieu tham chieu (Reference type)
+    // where T : new()
+    // Bat buoc T phai co constructor khong tham so (Rong, Mac Dinh)
+    public class GenericClass<T> where T : new()
+    {
+        public T Value { get; set; }
+        public T GetValue() 
+        { 
+            return Value; 
+        }
+        // Theo bang chu cai: T U V W X Y Z
+        // Generic method
+        public T Print<U>(U note)
+        {
+            Console.WriteLine(note);
+            return Value;
+        }
+    }
+}
